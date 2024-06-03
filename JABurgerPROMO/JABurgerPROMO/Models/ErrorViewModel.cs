b04930c0bd3fbb884ca61baf56d86892da249dad
Jimmy Albarracin <105869917+JimmyAlbarrac1n@users.noreply.github.com>
2024-06-02 19:01:16 -0500

    Delete JABurgerPROMO/JABurgerPROMO directory

--- JABurgerPROMO/JABurgerPROMO/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JABurgerPROMO.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
